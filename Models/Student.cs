709027ae2694669523390bd1e8870acc5478813b
mikus590 <38634735+mikus590@users.noreply.github.com>
2020-05-31 17:59:44 +0200

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cw3.ORMModels
+{
+    public partial class Student
+    {
+        public string IndexNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int IdEnrollment { get; set; }
+        public string Pswd { get; set; }
+        public string RefreshToken { get; set; }
+        public byte[] Salt { get; set; }
+
+        public Enrollment IdEnrollmentNavigation { get; set; }
+    }
+}
---
+++ Models/s17371Context.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Cw3.ORMModels
+{
+    public partial class s17371Context : DbContext
+    {
+        public s17371Context()
+        {
+        }
+
+        public s17371Context(DbContextOptions<s17371Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Enrollment> Enrollment { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+        public virtual DbSet<Studies> Studies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=db-mssql;Initial Catalog=s17371;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Enrollment>(entity =>
+            {
+                entity.HasKey(e => e.IdEnrollment);
+
+                entity.Property(e => e.IdEnrollment).ValueGeneratedNever();
+
+                entity.Property(e => e.StartDate).HasColumnType("date");
+
+                entity.HasOne(d => d.IdStudyNavigation)
+                    .WithMany(p => p.Enrollment)
+                    .HasForeignKey(d => d.IdStudy)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Enrollment_Studies");
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(e => e.IndexNumber);
+
+                entity.Property(e => e.IndexNumber)
+                    .HasMaxLength(100)
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.BirthDate).HasColumnType("date");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Pswd)
+                    .HasColumnName("pswd")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.RefreshToken)
+                    .HasColumnName("refreshToken")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Salt)
+                    .HasColumnName("salt")
+                    .HasMaxLength(32);
+
+                entity.HasOne(d => d.IdEnrollmentNavigation)
+                    .WithMany(p => p.Student)
+                    .HasForeignKey(d => d.IdEnrollment)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Student_Enrollment");
+            });
+
+            modelBuilder.Entity<Studies>(entity =>
+            {
+                entity.HasKey(e => e.IdStudy);
+
+                entity.Property(e => e.IdStudy).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+        }
+    }
+}
