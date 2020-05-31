b392f18cc0467924e2ca1c5b86a1671c8f1bdb28
mikus590 <38634735+mikus590@users.noreply.github.com>
2020-05-31 18:01:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Cw3.ORMModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Cw10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowStudent();
+            UpdateStudent();
+            Remove();
+
+        }
+
+        public static void ShowStudent()
+        {
+            var db = new s17371Context();
+            var s1 = db.Student.First();
+
+            s1.LastName = "LastNameUpdate";
+
+            db.SaveChanges();
+        }
+        public static void UpdateStudent()
+        {
+
+            var db = new s17371Context();
+
+            var res = db.Student.ToList();
+        }
+        public static void Remove() {
+
+            var db = new s17371Context();
+
+            var s2 = db.Student.OrderByDescending(s => s.IndexNumber).First();
+
+            db.Student.Remove(s2);
+
+            db.SaveChanges();
+        }
+    }
+}
